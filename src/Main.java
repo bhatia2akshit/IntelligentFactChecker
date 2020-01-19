459fd825d4f551628d88293b7772f9998803d308
bhatia2akshit <bhatia2akshit@gmail.com>
2020-01-19 19:34:45 +0100

    change the name of test file

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 	public static void main(String args[]) throws ClassNotFoundException, IOException {
 		AcceptTriplet obj = new AcceptTriplet();
-		String inputFile = "SNLP2019_training.tsv";
+		String inputFile = "SNLP2019_test.tsv";
 		String outputFile = "output.ttl";
 		obj.processing(inputFile, outputFile);
 	}
