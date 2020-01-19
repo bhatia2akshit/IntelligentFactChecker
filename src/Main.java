302018134c86b6512bae4c8047da37cdb33a43b0
bhatia2akshit <bhatia2akshit@gmail.com>
2020-01-19 18:59:34 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,5 @@ public class Main {
 		String inputFile = "SNLP2019_training.tsv";
 		String outputFile = "output.ttl";
 		obj.processing(inputFile, outputFile);
-//		obj.processing("/Users/akshitbhatia/Downloads/SNLP2019_training.tsv");
-//		obj.processFact("Ted Harbert is Chelsea Handler's better half");
-		// obj.processing("Ted Harbert is Akshit Bhatia's better half");
-		// New York City is IBM's innovation place.
-		// obj.processing("New York City is IBM's innovation place.");
 	}
 }
